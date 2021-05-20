899c31f3a80b3038704e7b81e377dc59ecddbb9e
Trung <trunga2t4@gmail.com>
2021-05-20 22:10:33 +0800

    task 3 Add Activity indicator

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bus arrival time:</Text>
-      <Text style={styles.timing}>{loading ? "Loading..." : "Loaded"}</Text>
+      <Text style={styles.timing}>
+        {loading ? <ActivityIndicator size="large" color="orange" /> : "Loaded"}
+      </Text>
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Refresh!</Text>
       </TouchableOpacity>
